ba6977e93abd46e49bf31b25b3a76d189175a95b
xloem <0xloem@gmail.com>
2021-12-15 17:31:38 +0000

    torch2tensormap.py converts parameters to a file that can be mmapped and instantly loads such a file

--- torch2tensormap.py
+++ torch2tensormap.py
@@ -1,6 +1,7 @@
 import torch, numpy as np
 import pickle
 import struct
+import mmap
 
 gptj_24G = '/media/3/huggingface/transformers/026f12960ffab80e6f4f983cd8672c6f4579e3a12d469f0cad87973e86376f78.45ab80c413af010231b34f81ca5a6a2fe0739bbe17b4672a5110f70bb75d2555'
 gpt2_1_5G = '/media/3/huggingface/transformers/6249eef5c8c1fcfccf9f36fc2e59301b109ac4036d8ebbee9c2b7f7e47f440bd.2538e2565f9e439a3668b981faf959c8b490b36dd631f3c4cd992519b2dd36f1'
@@ -17,8 +18,9 @@ class TensorMap:
         import os
         return os.path.exists(self.filename)
     def write(self, data):
+        self.pagesize = mmap.PAGESIZE
         with open(self.filename, 'wb') as output:
-            header = pickle.dumps((self.version, len(data)))
+            header = pickle.dumps((self.version, self.pagesize, len(data)))
             output.write(header)
             for name, tensor in data.items():
                 flat_tensor = tensor.flatten()
@@ -26,19 +28,30 @@ class TensorMap:
                 numpy_dtype = numpy.dtype
                 tensor_header = pickle.dumps((name, tensor.dtype, tuple(tensor.shape), numpy_dtype, len(numpy), tensor.requires_grad))
                 output.write(tensor_header)
+                pos = output.tell()
+                if pos % self.pagesize != 0:
+                    output.seek(pos - (pos % self.pagesize) + self.pagesize)
                 numpy.tofile(output)
     def read(self):
-        with open(self.filename, 'rb') as input:
-            self.version, data_len = pickle.load(input)
-            data = {}
-            for idx in range(data_len):
-                name, tensor_dtype, tensor_shape, numpy_dtype, numpy_len, requires_grad = pickle.load(input)
-                buf = input.read(numpy_dtype.itemsize * numpy_len)
-                numpy = np.frombuffer(buf, numpy_dtype, count=numpy_len, offset=0)
-                tensor = torch.from_numpy(numpy)
-                tensor = tensor.unflatten(0, tensor_shape)
-                tensor.requires_grad = requires_grad
-                data[name] = tensor
+        self.file = open(self.filename, 'rb')
+        self.version, self.pagesize, data_len = pickle.load(self.file)
+        assert self.pagesize % mmap.PAGESIZE == 0
+        data = {}
+        for idx in range(data_len):
+            name, tensor_dtype, tensor_shape, numpy_dtype, numpy_len, requires_grad = pickle.load(self.file)
+            pos = self.file.tell()
+            if pos % self.pagesize != 0:
+                pos += self.pagesize - (pos % self.pagesize)
+            #buf = self.file.read(numpy_dtype.itemsize * numpy_len)
+            bytelen = numpy_dtype.itemsize * numpy_len
+            buf = mmap.mmap(self.file.fileno(), bytelen, access = mmap.ACCESS_READ, offset = pos)
+
+            numpy = np.frombuffer(buf, numpy_dtype, count=numpy_len, offset=0)
+            tensor = torch.from_numpy(numpy)
+            tensor = tensor.unflatten(0, tensor_shape)
+            tensor.requires_grad = requires_grad
+            data[name] = tensor
+            self.file.seek(pos + bytelen)
         return data
 
 tensormap = TensorMap('test_output.tensormap')
