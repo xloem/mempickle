7982231893eabfce900c18865a1d78ee6a51fe3a
xloem <0xloem@gmail.com>
2021-12-26 16:18:21 +0000

    provide for autograd with matmul aarch64 patch

--- patch_pytorch.py
+++ patch_pytorch.py
@@ -42,8 +42,14 @@ if (
         if type(aliases) is str:
             aliases = aliases.split(' ')
         def patch(func):
+            name = func.__name__
+            try:
+                if issubclass(func, torch.autograd.Function):
+                    func = func.apply
+            except TypeError:
+                pass
             for module in modules:
-                setattr(module, func.__name__, func)
+                setattr(module, name, func)
                 for alias in aliases:
                     setattr(module, alias, func)
             return func
@@ -54,59 +60,78 @@ if (
         carray = (ctypes.c_char * bytect).from_address(t.data_ptr())
         return np.ndarray(shape=t.shape, buffer=carray, dtype=bfloat16.bfloat16)
     
-    @patch(torch, aliases='matmul')
-    def mm(mat1, mat2, *, out=None):
-        ## numpy matmul does not crash.  bfloat16 needs an additional package. ##
-        if out is None:
-            out = torch.empty((*mat1.shape[:-2], mat1.shape[-2], mat2.shape[-1]), dtype = mat1.dtype)
-        if mat1.dtype is torch.bfloat16:
-            mat1, mat2, npout = npbfloat16(mat1), npbfloat16(mat2), npbfloat16(out)
-            npout[:] = np.matmul(mat1, mat2)
-        else:
-            mat1, mat2, npout = mat1.numpy(), mat2.numpy(), out.numpy()
-            np.matmul(mat1, mat2, out=npout)
-        return out
-        ## mm crashes ##
-        ## einsum crashes ##
-        #if out is None:
-        #    out = torch.einsum('ij,jk->ik', mat1, mat2)
-        #else:
-        #    out[:] = torch.einsum('ij,jk->ik', mat1, mat2)
-        #return out
-        ## tensordot crashes ##
-        #if out is None:
-        #    out = torch.empty((mat1.shape[0], mat2.shape[1]))
-        #return torch.tensordot(mat1, mat2, dims=1, out=out)
-        ## inner crashes ##
-        #return torch.inner(mat1, mat2.T, out=out)
-        ## mv crashes ##
-        #if out is None:
-        #    out = torch.empty((mat1.shape[0], mat2.shape[1]))
-        #for colidx in range(out.shape[1]):
-        #    torch.mv(mat1, mat2[:, colidx], out = out[:, colidx])
-        #return out
-        ## 1d dot product does not crash as much but can still crash! ##
-        ## no crash experienced with bfloat16 but it was pretty slow ##
-        #if out is None:
-        #    out = torch.empty((mat1.shape[0], mat2.shape[1]), dtype = mat1.dtype)
-        #for mat1row, outrow in zip(mat1, out):
-        #    for mat2col, outpos in zip(mat2.T, outrow):
-        #        torch.dot(mat1row, mat2col, out=outpos)
-        #return out
-        ## broadcast multiplication does not crash, but can get large and slow ##
-        #if out is None:
-        #    out = (
-        #        # construct huge thing,
-        #        mat1.view(mat1.shape[0], 1, mat1.shape[1]) *
-        #        mat2.T.broadcast_to(mat1.shape[0], *mat2.T.shape)
-        #    ).sum(axis=2) # construct smaller thing
-        #else:
-        #    out[:] = (
-        #        # construct huge thing,
-        #        mat1.view(mat1.shape[0], 1, mat1.shape[1]) *
-        #        mat2.T.broadcast_to(mat1.shape[0], *mat2.T.shape)
-        #    ).sum(axis=2) # construct smaller thing
-        #return out
+    class mm(torch.autograd.Function):
+        @staticmethod
+        def backward(ctx, grad_output):
+            mat1, mat2 = ctx.saved_tensors
+            mat1_needs_grad, mat2_needs_grad = ctx.needs_input_grad
+            mat1_grad, mat2_grad = None, None
+            if grad_output is not None:
+                # ported from torch/csrc/autograd: FunctionsManual.cpp and generated/Functions.cpp
+                if mat1_needs_grad:
+                    mat1_grad = mm(grad_output, mat2.T.conj())
+                if mat2_needs_grad:
+                    mat2_grad = mm(mat1.T.conj(), grad_output)
+            return mat1_grad, mat2_grad
+        @staticmethod
+        #@torch.autograd.function.once_differentiable
+        def forward(ctx, mat1, mat2, *, out=None):
+            ctx.save_for_backward(mat1, mat2)
+            ctx.set_materialize_grads(False)
+
+            ## numpy matmul does not crash.  bfloat16 needs an additional package. ##
+            if out is None:
+                out = torch.empty((*mat1.shape[:-2], mat1.shape[-2], mat2.shape[-1]), dtype = mat1.dtype)
+            if mat1.dtype is torch.bfloat16:
+                npmat1, npmat2, npout = npbfloat16(mat1), npbfloat16(mat2), npbfloat16(out)
+                npout[:] = np.matmul(npmat1, npmat2)
+            else:
+                npmat1, npmat2, npout = mat1.detach().numpy(), mat2.detach().numpy(), out.numpy()
+                np.matmul(npmat1, npmat2, out=npout)
+            return out
+            ## mm crashes ##
+            ## einsum crashes ##
+            #if out is None:
+            #    out = torch.einsum('ij,jk->ik', mat1, mat2)
+            #else:
+            #    out[:] = torch.einsum('ij,jk->ik', mat1, mat2)
+            #return out
+            ## tensordot crashes ##
+            #if out is None:
+            #    out = torch.empty((mat1.shape[0], mat2.shape[1]))
+            #return torch.tensordot(mat1, mat2, dims=1, out=out)
+            ## inner crashes ##
+            #return torch.inner(mat1, mat2.T, out=out)
+            ## mv crashes ##
+            #if out is None:
+            #    out = torch.empty((mat1.shape[0], mat2.shape[1]))
+            #for colidx in range(out.shape[1]):
+            #    torch.mv(mat1, mat2[:, colidx], out = out[:, colidx])
+            #return out
+            ## 1d dot product does not crash as much but can still crash! ##
+            ## no crash experienced with bfloat16 but it was pretty slow ##
+            #if out is None:
+            #    out = torch.empty((mat1.shape[0], mat2.shape[1]), dtype = mat1.dtype)
+            #for mat1row, outrow in zip(mat1, out):
+            #    for mat2col, outpos in zip(mat2.T, outrow):
+            #        torch.dot(mat1row, mat2col, out=outpos)
+            #return out
+            ## broadcast multiplication does not crash, but can get large and slow ##
+            #if out is None:
+            #    out = (
+            #        # construct huge thing,
+            #        mat1.view(mat1.shape[0], 1, mat1.shape[1]) *
+            #        mat2.T.broadcast_to(mat1.shape[0], *mat2.T.shape)
+            #    ).sum(axis=2) # construct smaller thing
+            #else:
+            #    out[:] = (
+            #        # construct huge thing,
+            #        mat1.view(mat1.shape[0], 1, mat1.shape[1]) *
+            #        mat2.T.broadcast_to(mat1.shape[0], *mat2.T.shape)
+            #    ).sum(axis=2) # construct smaller thing
+            #return out
+
+    mm = patch(torch, aliases='matmul')(mm)
     
     @patch(torch)
     def addmm(input, mat1, mat2, *, beta=1, alpha=1, out=None):
