a1b39be1cfc64c40f8f0910a1e957c3cb7a22da8
xloem <0xloem@gmail.com>
2021-12-26 00:43:32 +0000

    more stable support for bfloat16 aarch64 matmul workaround

--- patch_pytorch.py
+++ patch_pytorch.py
@@ -1,11 +1,23 @@
 import torch
-import numpy as np
 import os
+import sys
 
-if os.uname().machine == 'aarch64' and not torch.cuda.is_available():
-    # some torch functions crash on low-end systems
-    # ( torch 1.10.1 https://github.com/pytorch/pytorch/issues/70350 )
-    # this module attempts to patch them to use numpy functions instead
+if (
+    os.uname().machine == 'aarch64'
+    and not torch.cuda.is_available()
+    and os.popen(f"{sys.executable} -c 'import torch; torch.mm(torch.randn((4096,4096)), torch.randn((4096,4096)))'").close() not in (None, 0, True)
+):
+    import warnings
+
+    warnings.warn(
+        '\ntorch matmul is crashing on your arch, likely due to blas compilation flags'
+        '\nsee e.g. https://github.com/pytorch/pytorch/issues/70350'
+        '\nsome functions will be patched to perform matmul via numpy'
+        '\nif you get an illegal instruction crash, some function was not patched'
+        '\nthis workaround uses ctypes and bfloat16 for half-precision math'
+    )
+
+    import bfloat16, ctypes, numpy as np
 
     def patch(*modules):
         def patch(func):
@@ -13,14 +25,65 @@ if os.uname().machine == 'aarch64' and not torch.cuda.is_available():
                 setattr(module, func.__name__, func)
             return func
         return patch
+
+    def npbfloat16(t):
+        bytect = len(t.flatten()) * 2
+        carray = (ctypes.c_char * bytect).from_address(t.data_ptr())
+        return np.ndarray(shape=t.shape, buffer=carray, dtype=bfloat16.bfloat16)
     
     @patch(torch)
     def mm(mat1, mat2, *, out=None):
+        ## numpy matmul does not crash.  bfloat16 needs an additional package. ##
         if out is None:
-            out = torch.einsum('ij,jk->ik', mat1, mat2)
+            out = torch.empty((mat1.shape[0], mat2.shape[1]), dtype = mat1.dtype)
+        if mat1.dtype is torch.bfloat16:
+            mat1, mat2, npout = npbfloat16(mat1), npbfloat16(mat2), npbfloat16(out)
+            npout[:] = np.matmul(mat1, mat2)
         else:
-            out[:] = torch.einsum('ij,jk->ik', mat1, mat2)
+            mat1, mat2, npout = mat1.numpy(), mat2.numpy(), out.numpy()
+            np.matmul(mat1, mat2, out=npout)
         return out
+        ## mm crashes ##
+        ## einsum crashes ##
+        #if out is None:
+        #    out = torch.einsum('ij,jk->ik', mat1, mat2)
+        #else:
+        #    out[:] = torch.einsum('ij,jk->ik', mat1, mat2)
+        #return out
+        ## tensordot crashes ##
+        #if out is None:
+        #    out = torch.empty((mat1.shape[0], mat2.shape[1]))
+        #return torch.tensordot(mat1, mat2, dims=1, out=out)
+        ## inner crashes ##
+        #return torch.inner(mat1, mat2.T, out=out)
+        ## mv crashes ##
+        #if out is None:
+        #    out = torch.empty((mat1.shape[0], mat2.shape[1]))
+        #for colidx in range(out.shape[1]):
+        #    torch.mv(mat1, mat2[:, colidx], out = out[:, colidx])
+        #return out
+        ## 1d dot product does not crash as much but can still crash! ##
+        ## no crash experienced with bfloat16 but it was pretty slow ##
+        #if out is None:
+        #    out = torch.empty((mat1.shape[0], mat2.shape[1]), dtype = mat1.dtype)
+        #for mat1row, outrow in zip(mat1, out):
+        #    for mat2col, outpos in zip(mat2.T, outrow):
+        #        torch.dot(mat1row, mat2col, out=outpos)
+        #return out
+        ## broadcast multiplication does not crash, but can get large and slow ##
+        #if out is None:
+        #    out = (
+        #        # construct huge thing,
+        #        mat1.view(mat1.shape[0], 1, mat1.shape[1]) *
+        #        mat2.T.broadcast_to(mat1.shape[0], *mat2.T.shape)
+        #    ).sum(axis=2) # construct smaller thing
+        #else:
+        #    out[:] = (
+        #        # construct huge thing,
+        #        mat1.view(mat1.shape[0], 1, mat1.shape[1]) *
+        #        mat2.T.broadcast_to(mat1.shape[0], *mat2.T.shape)
+        #    ).sum(axis=2) # construct smaller thing
+        #return out
     
     @patch(torch)
     def addmm(input, mat1, mat2, *, beta=1, alpha=1, out=None):
